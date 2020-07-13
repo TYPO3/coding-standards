dea8cf6649d9eaa7491ca269ca1f743d63d07782
Benni Mack <benni@typo3.org>
2020-07-13 17:19:36 +0200

    [TASK] Align coding guidelines with latest TYPO3 Core master

--- src/CsFixerConfig.php
+++ src/CsFixerConfig.php
@@ -31,15 +31,20 @@ EOF;
         '@DoctrineAnnotation' => true,
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'blank_line_after_opening_tag' => true,
+        'braces' => ['allow_single_line_closure' => true],
         'cast_spaces' => ['space' => 'none'],
+        'compact_nullable_typehint' => true,
         'concat_space' => ['spacing' => 'one'],
-        'declare_equal_normalize' => ['space' => 'single'],
+        'declare_equal_normalize' => ['space' => 'none'],
         'dir_constant' => true,
         'function_typehint_space' => true,
         'hash_to_slash_comment' => true,
         'lowercase_cast' => true,
+        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
         'modernize_types_casting' => true,
         'native_function_casing' => true,
+        'new_with_braces' => true,
         'no_alias_functions' => true,
         'no_blank_lines_after_phpdoc' => true,
         'no_empty_phpdoc' => true,
@@ -69,7 +74,8 @@ EOF;
         'phpdoc_types_order' => ['null_adjustment' => 'always_last', 'sort_algorithm' => 'none'],
         'return_type_declaration' => ['space_before' => 'none'],
         'single_quote' => true,
-        'whitespace_after_comma_in_array' => true
+        'single_trait_insert_per_statement' => true,
+        'whitespace_after_comma_in_array' => true,
     ];
 
     public function __construct($name = 'TYPO3')
