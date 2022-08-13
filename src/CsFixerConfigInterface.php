f5cebc154a8d8249a6009ead3b5f0a275b38bdb3
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2022-08-13 13:17:14 +0200

    [TASK] Proper creation of inherited classes (#52)

---
+++ src/CsFixerConfigInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 project.
+ *
+ * (c) 2019-2022 Benni Mack
+ *               Simon Gilli
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CodingStandards;
+
+interface CsFixerConfigInterface
+{
+    public function __construct(string $name);
+}
