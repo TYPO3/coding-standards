95e602f777b26d5c59c1775ef8f8a23bc1be671a
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2023-06-02 15:19:14 +0200

    [TASK] Add tests for abstract Command class (#97)

---
+++ tests/Unit/Console/Command/CommandTestImplementation.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 project.
+ *
+ * (c) 2019-2022 Benni Mack
+ *               Simon Gilli
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CodingStandards\Tests\Unit\Console\Command;
+
+use TYPO3\CodingStandards\Console\Command\Command;
+
+/**
+ * @internal for testing only
+ */
+final class CommandTestImplementation extends Command
+{
+}
