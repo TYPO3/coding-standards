6f1df4d379647a5ab6798a4455f04580405c2127
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2023-06-03 15:41:44 +0200

    [TASK] Update year in headers (#101)

--- tests/Unit/Console/Command/UpdateCommandTest.php
+++ tests/Unit/Console/Command/UpdateCommandTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the TYPO3 project.
  *
- * (c) 2019-2022 Benni Mack
+ * (c) 2019-2023 Benni Mack
  *               Simon Gilli
  *
  * For the full copyright and license information, please view
