c7a8fc1da14bff6c0301f15a7c968571e318976b
Benni Mack <benni@typo3.org>
2026-07-24 10:09:57 +0200

    [!!!][TASK] Raise the minimum PHP version to 8.2
    PHP 8.1 reached its end of life in December 2025. Raise the platform
    requirement accordingly and update the CI matrices, the Rector rule
    sets and the DDEV configuration.

--- rector.php
+++ rector.php
@@ -41,8 +41,8 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-        DowngradeLevelSetList::DOWN_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
+        DowngradeLevelSetList::DOWN_TO_PHP_82,
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
         SetList::DEAD_CODE,
